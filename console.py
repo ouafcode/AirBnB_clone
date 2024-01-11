8668e11c44dfb966dc99d89ca1bfb7b8b9ce69e4
ouafcode <ouafaesaim@gmail.com>
2024-01-11 13:42:10 -0800

    task7

--- console.py
+++ console.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """ program for command interpreter """
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
-    """ define command class """
+    """ define command class 
+    Attribute:
+        prompt (str): command prompt showed 
+    """
+
     prompt = "(hbnb)"
+    __classe  = {
+        "BaseModel"
+    }
 
     def do_quit(self, arg):
         """ Quit command to exit the program """
@@ -20,6 +28,60 @@ class HBNBCommand(cmd.Cmd):
         """ shouldn’t execute anything """
         pass
 
+    def do_create(self, arg):
+        """ create new class instance """
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.__classe:
+            print("** class doesn't exist **")
+        else:
+            print(arg[0].id)
+            storage.save()
+
+    def do_show(self, arg):
+        """ print string representation of instance
+            base on class name and id
+        """
+        obj_dict = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.__classe:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) not in obj_dict:
+            print("** no instance found **")
+        else:
+            print(obj_dict["{}.{}".format(arg[0], arg[1])])
+
+    def do_destroy(self, arg):
+        """ Deletes an instance based on the class name and id """
+        obj_dict = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.__classe:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) not in obj_dict:
+            print("** no instance found **")
+        else:
+            del obj_dict["{}.{}".format(arg[0], arg[1])]
+            storage.save()
+
+    def do_all(self, arg):
+        """ Prints all string representation of all instances """
+        if arg[0] not in HBNBCommand.__classe:
+            print("** class doesn't exist **")
+        else:
+            obj_dit = []
+            for obj in storage.all().values():
+                if len(arg) > 0 and arg[0] == obj.__class__.__name__:
+                    obj_dict.append(obj.str())
+                elif len(arg) == 0:
+                    obj_dict.append(obj.__str__())
+            print(obj_dit)
+            
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,7 @@ import json
 import datetime
 
 
-class FileStorage():
+class FileStorage:
     """ Define class filestorage """
     __file_path = "files.json"
     __objects = {}
@@ -22,14 +22,17 @@ class FileStorage():
     def save(self):
         """ serializes __objects to the JSON file """
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-            json.dump(d, f)
+            dict1 = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(dict1, f)
 
     def reload(self):
         """  deserializes the JSON file to __objects """
         if not os.path.isfile(FileStorage.__file_path):
             return
         with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
-            dict_objt = json.load(f)
-            dict_objt = {k: v for k, v in dict_objt.values()}
-            FileStorage.__objects = dict_objt
+            dict1 = json.load(f)
+            for key, value in dict1.items():
+                class_name, obj_id = key.split('.')
+                class_ = globals()[class_name]
+                obj = class_(**value)
+                FileStorage.__objects[key] = obj
