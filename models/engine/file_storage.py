9c5bf3ca58292bd0b0a767283d18e100bd71b97d
ouafcode <ouafaesaim@gmail.com>
2024-01-13 12:37:53 -0800

    task3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@
 
 import json
 import datetime
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.amenity import Amenity
@@ -13,7 +14,7 @@ from models.review import Review
 from models.state import State
 
 
-class FileStorage():
+class FileStorage:
     """ Define class filestorage """
 
     __file_path = "file.json"
@@ -34,15 +35,27 @@ class FileStorage():
             dict1 = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(dict1, f)
 
+    def cls_dict(self):
+        """ Return a dict of input classes """
+
+        cls_dict = {"BaseModel": BaseModel,
+                    "User": User,
+                    "State": State,
+                    "City": City,
+                    "Amenity": Amenity,
+                    "Place": Place,
+                    "Review": Review}
+        return cls_dict
+
     def reload(self):
         """  deserializes the JSON file to __objects """
-        try:
-            with open(FileStorage.__file_path, mode='r') as f:
-                obj_dict = json.load(f)
-            for k, v in obj_dict.items():
-                cls_name = v.get('__class__')
-                obj = cls_name + '(**v)'
-                FileStorage.__objects[k] = obj
+        if not os.path.isfile(FileStorage.__file_path):
+            return
 
-        except FileNotFoundError:
-            pass
+        with open(FileStorage.__file_path, mode='r') as f:
+            obj_dict = json.load(f)
+            obj = {}
+            for k, v in obj_dict.items():
+                if "__class__" in v:
+                    obj[k] = self.classes().get(v["__class__"], object)(**v)
+            FileStorage.__objects = obj
