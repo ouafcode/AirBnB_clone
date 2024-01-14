87799e4a01eccaaba742ecf604f2fece0231f6ff
ouafcode <ouafaesaim@gmail.com>
2024-01-14 05:23:53 -0800

    task5

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -36,3 +36,19 @@ class TestFileStorage(unittest.TestCase):
         my_model.name = "My_First_Model"
         my_model.my_number = 89
         my_model.save()
+
+    def test_instances(self):
+        """check instance"""
+        obj = FileStorage()
+        self.assertIsInstance(obj, FileStorage)
+
+    def test_docs(self):
+        """Test doc"""
+        self.assertIsNotNone(FileStorage.all)
+        self.assertIsNotNone(FileStorage.new)
+        self.assertIsNotNone(FileStorage.save)
+        self.assertIsNotNone(FileStorage.reload)
+
+
+if __name__ == '__main__':
+    unittest.main()
