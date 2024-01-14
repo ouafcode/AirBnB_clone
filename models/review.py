ac01f161b9eac2a13603770727bc9e402f07757f
kawtar <kawtarennabirha@gmail.com>
2024-01-14 07:26:46 -0800

    task

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Define Class Review"""
+    """Define Class Review"""
     place_id = ""
     user_id = ""
     text = ""
